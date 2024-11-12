d42d89e73ce67779a1b483d82ccc3ac509759605
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2024-11-12 16:37:35 +0330

    add q6_for_list.sh

--- docker_ubuntu/q6_for_list.sh
+++ docker_ubuntu/q6_for_list.sh
@@ -2,30 +2,40 @@
 
 h=("f1" "f2" "f3")
 
-for r in "${h[@]}"; do
+for r in ${h[@]}; do
     echo $r
 done
+
+echo ------
 
 for r in s1 s2 s3; do
     echo $r
 done
 
+echo ------
+
 for r in {1..3}; do
     echo $r
 done
+
+echo ------
 
 for ((i = 0; i < 3; i++)); do
     echo $i
 done
 
+echo ------
+
 h=({1..3})
 
-echo "${h[2]}"
+echo ${h[1]}
 
-for r in "${h[@]}"; do
+for r in ${h[@]}; do
     echo $r
 
 done
+
+echo ------
 
 h=(1 2 3 4)
 h+=(5 6)
