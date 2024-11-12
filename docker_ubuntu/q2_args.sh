f5529e5851965b12bb07d277f60e5aef9b918124
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2024-11-12 15:57:12 +0330

    add q2_args.sh

---
+++ docker_ubuntu/q2_args.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+arg1=$1
+arg2=$2
+
+echo "first argument: $1"
+echo "second argument: $2"
+
+echo "script name: $0"
+
+echo "all arguments: $*"
+echo "number of arguments: $#"
+
+echo "all arguments for loop: $@"
