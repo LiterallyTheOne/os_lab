da3758508484bbfc60cf7df4e228c19a29e66c64
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2024-11-13 12:58:43 +0330

    Add another way to deal with numbers in conditions

--- docker_ubuntu/q3_if.sh
+++ docker_ubuntu/q3_if.sh
@@ -7,3 +7,11 @@ elif [ $1 -eq $2 ]; then
 else
     echo "lesser"
 fi
+
+# if (($1 > $2)); then
+#     echo "greater"
+# elif (($1 == $2)); then
+#     echo "equal"
+# else
+#     echo "lesser"
+# fi
